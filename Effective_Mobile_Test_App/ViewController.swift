aaeeb263c6429c29adb1c02a65e8843e4f2ccbb1
AntonDemidenko <putiatish@gmail.com>
2025-01-29 10:58:42 +0100

    Initial Commit

---
+++ Effective_Mobile_Test_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Effective_Mobile_Test_App
+//
+//  Created by Anton Demidenko on 29.1.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
